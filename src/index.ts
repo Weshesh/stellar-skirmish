71216eb68ca1f2446d78b837c8f412a1db354a09
Kozjat <arvo@saalits.com>
2019-10-31 07:52:53 +0200

    Small refactor

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,31 @@ import * as PIXI from 'pixi.js'
 import {Board} from "./board";
 
 class Main {
-    activeKey = "1";
-    colorKey = 0x85c7f2;
-    app: PIXI.Application;
-    
-    constructor() {
-        document.body.onkeypress = ({charCode, key}) => {
-            if (charCode >= 49 && charCode <= 54 || charCode == 32 ) {
-                switch (charCode) {
-                    case 32:
-                    if (this.colorKey == 0xf28590) {
-                        this.colorKey = 0x85c7f2;
-                    }
-                    else {
-                        this.colorKey = 0xf28590;
-                    }
-                    break;
-                    default:
-                    this.activeKey = key;
+    private config = {
+        activeKey: "1",
+        colorKey: 0x85c7f2
+    };
+
+    private readonly app: PIXI.Application;
+
+    private keyPressEvent({charCode, key}: KeyboardEvent) {
+        if (charCode >= 49 && charCode <= 54 || charCode == 32) {
+            if (charCode === 32) {
+                if (this.config.colorKey == 0xf28590) {
+                    this.config.colorKey = 0x85c7f2;
                 }
+
+                this.config.colorKey = 0xf28590;
+                return;
             }
-        };
-        
+
+            this.config.activeKey = key;
+        }
+    }
+
+    constructor() {
+        document.body.onkeypress = this.keyPressEvent.bind(this);
+
         this.app = new PIXI.Application({
             backgroundColor: 0xFFFFFF,
             antialias: true,
@@ -31,18 +34,19 @@ class Main {
             height: window.innerHeight,
         });
         document.body.appendChild(this.app.view);
-        
+
         this.createBoard();
-        
-        // Animation loop
-        this.app.ticker.add((delta) => {
-            // circle.position.x += delta * 0.3;
-        });
+
+        this.app.ticker.add(this.animationLoop.bind(this));
     }
-    
+
+    private animationLoop(delta: number) {
+        // circle.position.x += delta * 0.3;
+    }
+
     private createBoard() {
         const board = Board.generate(this.app);
-        
+
         this.app.stage.addChild(board);
     }
 }
