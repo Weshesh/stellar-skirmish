fba8a5ca4c181e56b8d8745df67fcb6d2750e67e
Andi Angel <andi.angel@boheemia.ee>
2019-11-13 22:19:30 +0200

    Boar constants changed. Spaces are even now.

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,6 @@ export namespace Config {
     export const yPos = 0;
     export const xPos = 0;
     export const side = 0;
-    export const xStep = Hexagon.config.hexRadius + 10;
-    export const yStep = Hexagon.config.hexHeight * 3;
+    export const xStep = Hexagon.config.xStep + 5;
+    export const yStep = Hexagon.config.yStep*3 + 7;
 }
