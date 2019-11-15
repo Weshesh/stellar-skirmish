43514a9e58490a12f180f4d6379be63e342a5cdd
Andi Angel <andi.angel@boheemia.ee>
2019-11-15 18:27:11 +0200

    keyPress event working, fixing g.Var. access

--- src/globalVariables.ts
+++ src/globalVariables.ts
@@ -1,20 +1,18 @@
 export namespace globalVariables {
-    export class Variables {
-        private data = {
-            activeKey: "1",
-            activeColor: "0x85c7f2"
-        }
-        public setActiveKey(input) {
-            this.data.activeKey = input;
-        }
-        public setActiveColor(input) {
-            this.data.activeColor = input;
-        }
-        public getActiveKey() {
-            return this.data.activeColor;
-        }
-        public getActiveColor() {
-            return this.data.activeColor;
-        }
+    let variables = {
+        activeKey: "1",
+        activeColor: "0x85c7f2"
+    }
+    export function setActiveKey(input) {
+        this.variables.activeKey = input;
+    }
+    export function setActiveColor(input) {
+        this.variables.activeColor = input;
+    }
+    export function getActiveKey() {
+        return this.variables.activeColor;
+    }
+    export function getActiveColor() {
+        return this.variables.activeColor;
     }
 }
--- src/keyboardEvents.ts
+++ src/keyboardEvents.ts
@@ -2,17 +2,15 @@ import * as PIXI from 'pixi.js'
 import { globalVariables } from './globalVariables'
 
 export namespace keyboardEvents {
-    export class keyPress{
-        onKeyPress({charCode, key}: KeyboardEvent) {
-            if (charCode >= 49 && charCode <= 54 || charCode == 32) {
-                if (charCode === 32) {
-                    const spacePressed = new globalVariables.Variables().getActiveColor;
-                    if (spacePressed == "0xf28590") {
-                        const = new globalVariables.Variables.setActiveKey(0x85c7f2);
-                        
-                        return;
-                    }
+        export function keyPress({ charCode, key }: KeyboardEvent) {
+        if (charCode >= 49 && charCode <= 54 || charCode == 32) {
+            if (charCode === 32) {
+                if (globalVariables.getActiveColor() === "0xf28590") {
+                    globalVariables.setActiveColor("0x85c7f2");
+                    return;
                 }
+                else 
+                globalVariables.setActiveColor("0xf28590");
             }
         }
     }
