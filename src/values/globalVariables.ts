94f1d9a6f440c4919ff9ca563f7087e390110c35
Andi Angel <andi.angel@boheemia.ee>
2019-11-17 13:09:17 +0200

    input typing

--- src/values/globalVariables.ts
+++ src/values/globalVariables.ts
@@ -5,10 +5,10 @@ export namespace globalVariables {
     let activeKey: string = "1";
     let activeColor: number = globalConstants.colors.blue;
     
-    export function setActiveKey(input) {
+    export function setActiveKey(input: number) {
         this.activeKey = input;
     }
-    export function setActiveColor(input) {
+    export function setActiveColor(input: number) {
         this.activeColor = input;
     }
     export function getActiveKey() {
