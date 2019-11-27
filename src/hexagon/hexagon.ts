d51e549e8781509efc6a8585c604d649657c2d90
Andi Angel <andi.angel@boheemia.ee>
2019-11-27 17:31:37 +0200

    Trigonometric solution of hex generation

--- src/hexagon/hexagon.ts
+++ src/hexagon/hexagon.ts
@@ -9,28 +9,21 @@ export namespace Hexagon {
         xStep: 34.64,
         yStep: 20,
         side: 40,
+        hexagonDiameter: 40,
+        circleStart : Math.PI*2/6,
+        sineStep : Math.PI / 3,
+        fullCircle : Math.PI * 2, 
     };
 
     export function create(positionX: number, positionY: number) {
         let x = 0, y = 0;
-
         const shape = new Graphics();
         shape.lineStyle(4, 0xeaedec);
-        shape.moveTo(x, y - config.side)
-        shape.lineTo(x + config.xStep, y - config.yStep);
-        shape.lineTo(x + config.xStep, y + config.yStep);
-        shape.lineTo(x, y + config.yStep * 2);
-        shape.lineTo(x - config.xStep, y + config.yStep);
-        shape.lineTo(x - config.xStep, y - config.yStep);
-        shape.lineTo(x, y - config.side);
-        shape.lineTo(x + config.xStep, y - config.yStep);
-        /*
-        	for(let i = 0; i <= 360; i+=60) {
-                xCurrent = xInterval*sin(ideg); radians?
-                yCurrent = yInterval*cos(ideg);
-                shape.lineTo(xCurrent, yCurrent);
+        	for(let index = config.circleStart; index <= config.fullCircle + config.sineStep; index += config.sineStep) {
+                x += Math.sin(index)*40;
+                y += Math.cos(index+Math.PI)*40;
+                shape.lineTo(x, y);
             }
-        */
         shape.tint = globalConstants.colors.grey;
 
         shape.interactive = true;
