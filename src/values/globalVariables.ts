a371d6481b594dc921212700b4062543e9daa077
Andi Angel <andi.angel@boheemia.ee>
2019-11-27 16:05:08 +0200

    getActiveKey() fixed, returns proper values

--- src/values/globalVariables.ts
+++ src/values/globalVariables.ts
@@ -5,16 +5,19 @@ export namespace globalVariables {
     let activeKey: string = "1";
     let activeColor: number = globalConstants.colors.blue;
     
-    export function setActiveKey(input: number) {
+    export function setActiveKey(input: string) {
         this.activeKey = input;
     }
     export function setActiveColor(input: number) {
         this.activeColor = input;
     }
     export function getActiveKey() {
-        return this.activeColor;
+        return this.activeKey;
     }
     export function getActiveColor() {
         return this.activeColor;
     }
 }
+
+
+
